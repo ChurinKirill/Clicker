f16b2ad0c96ae40bcdb212e9c8c611f84c534b57
Kirill Kirill <74719945+ChurinKirill@users.noreply.github.com>
2022-03-30 18:48:53 +0300

    Update Clicker1.py

--- Clicker1.py
+++ Clicker1.py
@@ -5,7 +5,7 @@ import pyautogui
 import keyboard
 
 class Clicker1(Clicker):
-    """Clicker which clicks on corrent mouse position with interval n seconds"""
+    """Clicker which clicks on corrent mouse position with interval -n- seconds"""
     def __init__(self, stopbutton: str, delayTime: int):
         super().__init__(stopbutton)
         self.delayTime: int = delayTime
